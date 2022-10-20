c66ea03471c21a73bb29c4540fbffca9a805b47b
박민성 <102217591+minsung060607@users.noreply.github.com>
2022-10-20 23:24:49 +0900

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+print(type('egoing')) #<class 'str'>
+name = 'egoing'
+print(name) #egoing
+print(type(['egoing', 'leezche', 'graphittie'])) #<class 'list'>
+names = ['egoing', 'leezche', 'graphittie']
+print(names) 
+print(names[2]) #graphittie
+egoing = ['programmer', 'seoul', 25, False]
+egoing[1] = 'busan'
+print(egoing) #['programmer', 'busan', 25, False]
